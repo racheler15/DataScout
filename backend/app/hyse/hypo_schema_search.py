a80340976b9e595e8329aa2cd29adf5068f60fba
Rachel Lin <raelin@berkeley.edu>
2025-03-26 12:48:34 -0700

    updated baseline

--- backend/app/hyse/hypo_schema_search.py
+++ backend/app/hyse/hypo_schema_search.py
@@ -240,7 +240,7 @@ def aggregate_hyse_search_results(results):
         db_name = result['database_name'].lower()
         logging.info(db_name)
 
-        keywords = ["building data genome", "georgia voter lists", "ohio census data", "book recommendation", "stock"]
+        keywords = ["building data genome", "georgia voter lists", "ohio census data"]
 
         if any(keyword in db_name for keyword in keywords):            
             logging.info("SKIPPING")
